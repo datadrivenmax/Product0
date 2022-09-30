ca13cc90a8d322cacc8fe7f8dfe565a485cd4971
Max <74901101+datadrivenmax@users.noreply.github.com>
2022-09-29 18:34:13 -0700

    add banner files

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Banner.css'
 
+
 function Banner() {
-  return (
-    <div className='banner'>
-        <p1>Banner</p1>
-    </div>
-  )
+    return (
+        <div className='banner'>
+            <div className='banner__info'>
+                <h1>Get out and stretch your imagination</h1>
+                <h5>Plan a different kind of getaway to uncover the hideen gems near you</h5>
+                <button variant="outlined">
+                    Explore Nearby
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Banner
--- airbnb-clone/src/Home.js
+++ airbnb-clone/src/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Home.css'
-import Banner from  './Banner.js'
+import Banner from './Banner.js'
 
 function Home() {
   return (
     <div>
         
-        <h1>Home Component</h1>
+        <h1>Clone</h1>
         <Banner/>
 
     </div>
