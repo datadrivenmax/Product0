3e4bb26b9f8ff4c4a21e2d0cc487a461fd31ed08
Max <74901101+datadrivenmax@users.noreply.github.com>
2022-10-01 17:38:12 -0700

    add searchPage buttons. material ui icons still not working

--- airbnb-clone/src/SearchPage.js
+++ airbnb-clone/src/SearchPage.js
@@ -1,11 +1,21 @@
 import './SearchPage.css'
 import React from 'react'
 
+import { Button } from "@material-ui/core";
+
 function SearchPage() {
   return (
     <div className='searchPage'>
-      <div className='searchPage'>
-        <h1>Hi my name is search</h1>
+      <div className='searchPage__info'>
+        <p>62 stays · 26 august to 30 august · 2 guest</p>
+        <h1>Stays nearby</h1>
+        <button><h6>Cancellation Flexibility</h6></button>
+        <button><h6>Type of Place</h6></button>
+        <button><h6>Price</h6></button>
+        <button><h6>Rooms and beds</h6></button>
+        <button><h6>More filters</h6></button>
+
+
       </div>
     </div>
   )
